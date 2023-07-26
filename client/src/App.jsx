d68425fca9435a579030bad29ab2dcb1d34c5850
hyeonah <hyeonah.hello@gmail.com>
2023-07-25 20:16:15 -0400

    Create Layout and Header and Add Layout router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
+import Layout from './Layout'
 import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
     <Routes>
       <Route
-        index
         path='/'
-        element={<IndexPage />}
-      />
+        element={<Layout />}>
+        <Route
+          index
+          element={<IndexPage />}
+        />
+        <Route
+          path='/login'
+          element={<LoginPage />}
+        />
+      </Route>
     </Routes>
   )
 }
