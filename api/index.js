3b6072ee93e9bc13ae64b6c387572eeb8ed29877
hyeonah <hyeonah.hello@gmail.com>
2023-08-08 16:46:19 -0400

    Install dotenv and mongoose package and Create UserModel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import cors from 'cors'
+import 'dotenv/config'
 import express from 'express'
+import mongoose from 'mongoose'
+import { UserModel } from './models/User.js'
 
 const app = express()
+
+app.use(express.json())
 
 app.use(
   cors({
@@ -10,8 +15,20 @@ app.use(
   })
 )
 
+mongoose.connect(process.env.MONGO_URL)
+
 app.get('/test', (req, res) => {
   res.json('test ok')
+})
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body
+  UserModel.create({
+    name,
+    email,
+    password,
+  })
+  res.json({ name, email, password })
 })
 
 app.listen(4000, () => {
